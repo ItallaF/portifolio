f8307b505e03e540849c6dcdc0ec87d444b4a133
Italla Felyne <itallafelynemf@gmail.com>
2026-02-16 15:14:20 -0300

    Fix - convert styledComponents in css in header

--- src/Componets/Header/Header.tsx
+++ src/Componets/Header/Header.tsx
@@ -1,45 +1,44 @@
 import React, { memo, useState } from 'react';
-import { HeaderStyled, ImgLogo, LogoStyled, MenuContentStyled, MenuStyled } from './HeaderStyle';
-import { FaBars } from 'react-icons/fa6';
+import styles from './Header.module.css';
+import { FaBars } from 'react-icons/fa';
 import logo from '../../assets/img/Logo.jpg';
 
 function Header() {
-  const [show, setShow] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const toggleMenu = () => {
-    setShow(!show)
+    setIsOpen(prev => !prev)
   }
 
   return (
-    <HeaderStyled>
-      <LogoStyled>
-        <button onClick={() => window.location.href = '/'}>
-          <ImgLogo src={logo} alt="Logo" />
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <button className={styles.logoButton} onClick={() => window.location.href = '/'}>
+          <img src={logo} alt="Logo" />
         </button>
-      </LogoStyled>
-
-      <MenuContentStyled>
-        <FaBars onClick={toggleMenu} style={{color: '#f5fffa', height: '7vh', width: '27vw'}}/>
-      </MenuContentStyled>
-
-      <MenuStyled show={show}>
-        <li>
-          <a href='#home'>Home</a>
-        </li>
-        <li>
-          <a href='#sobreMim'>Sobre mim</a>
-        </li>
-        <li>
-          <a href='#experiencias'>Experiências</a>
-        </li>
-        <li>
-          <a href='#projetos'>Projetos</a>
-        </li>
-        <li>
-          <a href='#certificacoes'>Certificações</a>
-        </li>
-      </MenuStyled>
-    </HeaderStyled>
+       <nav className={styles.navDesktop}>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#sobreMim">Sobre mim</a></li>
+            <li><a href="#experiencias">Experiências</a></li>
+            <li><a href="#projetos">Projetos</a></li>
+            <li><a href="#certificacoes">Certificações</a></li>
+          </ul>
+        </nav>
+        <button className={styles.hamburger}>
+          <FaBars onClick={toggleMenu} style={{color: '#f5fffa', height: '7vh', width: '27vw'}}/>
+        </button>
+      </div>
+      <nav className={`${styles.mobileMenu} ${ isOpen ? styles.open : ''}`}>
+        <ul>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#sobreMim'>Sobre mim</a></li>
+          <li><a href='#experiencias'>Experiências</a></li>
+          <li><a href='#projetos'>Projetos</a></li>
+          <li><a href='#certificacoes'>Certificações</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 export default memo(Header);
