70eaca761f98513067a1809570efb1e166088d79
Italla Felyne <itallafelynemf@gmail.com>
2026-02-16 15:50:14 -0300

    Fix- convert styledComponents in css in Banner

--- src/Componets/Banner/Banner.tsx
+++ src/Componets/Banner/Banner.tsx
@@ -1,31 +1,41 @@
+import styles from './Banner.module.css';
 import { Linkedin, GitHub } from '../../Icons';
-import { BannerDiv, BannerSection, Button, ButtonDiv, ButtonLink, ButtonSvg, TextBanner, TitleBanner } from './BannerStyle';
 
 export function Banner() {
   return (
-    <BannerSection id='home'>
-      <BannerDiv>
-        <TitleBanner>Italla Felyne</TitleBanner>
-        <TextBanner>Desenvolvedora Front-End e Mobile</TextBanner>
-        <ButtonDiv>
-          <ButtonLink href='https://www.linkedin.com/in/italla-felyne/'>
-            <Button>
-              Linkedin
-              <ButtonSvg>
-                <Linkedin />
-              </ButtonSvg>
-            </Button>
-          </ButtonLink>
-          <ButtonLink href='https://github.com/ItallaF'>
-            <Button>
-              GitHub
-              <ButtonSvg>
-                <GitHub />
-              </ButtonSvg>
-            </Button>
-          </ButtonLink>
-        </ButtonDiv>
-      </BannerDiv>
-    </BannerSection>
+    <section id="home" className={styles.banner}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Italla Felyne</h1>
+        <p className={styles.subtitle}>
+          Desenvolvedora Front-End e Mobile
+        </p>
+
+        <div className={styles.buttons}>
+          <a
+            className={styles.button}
+            href="https://www.linkedin.com/in/italla-felyne/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+            <span className={styles.icon}>
+              <Linkedin />
+            </span>
+          </a>
+
+          <a
+            className={styles.button}
+            href="https://github.com/ItallaF"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+            <span className={styles.icon}>
+              <GitHub />
+            </span>
+          </a>
+        </div>
+      </div>
+    </section>
   );
-};
+}
